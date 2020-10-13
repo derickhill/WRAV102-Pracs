5d17dbbc1e9bf89baef925425b4d6d4104706f82
Derick Hill <s221419535@mandela.ac.za>
2020-10-13 10:23:28 +0200

    Practical 2 added.

---
+++ Prac02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Prac02
+{
+    class Program
+    {
+        static void addMember(Member[] List, ref int nrEl)
+        {
+            Console.WriteLine("Name of member:");
+            List[nrEl] = new Member(Console.ReadLine());
+            nrEl++;
+        }
+        static void Main(string[] args)
+        {
+            Member[] List = new Member[10];
+            int nrEl = 0;
+
+            for(int i = 0; i < 3; i++)
+            {
+                addMember(List, ref nrEl);
+            }
+
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(List[i].getName());
+            }
+
+            for(int i = 0; i < 3; i++)
+            {
+                List[i].setPoints(30);
+            }
+        }
+    }
+}
